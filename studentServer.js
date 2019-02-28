745ca92220faf4954d33ca7ba3b2f2c3714466e2
ayue19960816 <uceslim@ucl.ac.uk>
2019-02-28 05:00:16 -0500

    connect to database via server and get the file

--- studentServer.js
+++ studentServer.js
@@ -10,6 +10,44 @@ res.header("Access-Control-Allow-Headers", "X-Requested-With");
 next();
 });
 
+//process the uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+
+//set up a database connection
+var fs = require('fs');
+var pg = require('pg');
+var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+var split = configarray[i].split(':');
+config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+
+//get the database connection
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
+res.status(400).send(err);
+}
+client.query('SELECT name FROM london_poi' ,function(err,result) {
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
+});
+});
+});
+
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
