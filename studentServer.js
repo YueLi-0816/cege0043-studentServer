e5e0c0d1e2a8e153f551af249368056c9f3e8d94
ayue19960816 <uceslim@ucl.ac.uk>
2019-02-28 04:08:39 -0500

    change the server to use the Express Static Routes to call files

--- studentServer.js
+++ studentServer.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require("path");
 var app = express();
 
+
+
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
@@ -13,13 +15,6 @@ app.get('/',function (req,res) {
 res.send("hello world from the HTTP server");
 });
 
-app.get('/:fileName', function (req, res) {
-// run some server-side code
-var fileName = req.params.fileName;
-console.log(fileName + ' requested');
-// note that __dirname gives the path to the studentServer.js file
-res.sendFile(__dirname + '/'+ fileName);
-});
 
 // adding functionality to log the requests
 app.use(function (req, res, next) {
@@ -29,6 +24,6 @@ console.log("The file " + filename + " was requested.");
 next();
 });
 
-
-
-
+// serve static files - e.g. html, css
+// this should always be the last line in the server file
+app.use(express.static(__dirname));
